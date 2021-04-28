7e63bc1dcf336d48875af6fae51830fe05e05ba0
Илья Гагин <neyzer-ilya@outlook.com>
2021-04-28 10:41:50 +0700

    init

---
+++ HashConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HashConsole
+{
+    class Program
+    {
+        static readonly Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите мат. ожидание:");
+            double lambda = 1.0 / Convert.ToDouble(Console.ReadLine());
+            var table = new HashTable.HashTable();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int val = (int)Math.Round(GenerateExp(lambda), 0);
+                try
+                {
+                    var col = table.Add(val);
+                    Console.WriteLine($"Добавили число: {val}, кол-во коллизий: {col}, сумма: {table.Count}");
+                }
+                catch (ArgumentException) { i--; }
+            }
+        }
+
+        private static double GenerateExp(double lambda)
+        {
+            return -Math.Log(rnd.NextDouble()) / lambda;
+        }
+    }
+}
